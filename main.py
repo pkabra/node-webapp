939cd6477bf7b75ceef9f263ba8a5df6a0ad8496
Pratik Kabra <pratik@kabra.com>
2018-10-16 12:02:28 -0700

    Bad PY file testing

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+def unused_func():
+    print 'does nothing'
+
+def main():
+    print "This is a really bad python file"
+    unused_var = 'bad data'
+
+if __name__ == '__main__':
+    main()
