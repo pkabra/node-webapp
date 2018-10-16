b8eb1c4df846a0d708f0ba878a9fa9faefc901a3
Pratik Kabra <pratik@kabra.com>
2018-10-16 11:45:42 -0700

    Some bad changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const unsusedVariable = 1;
+
+const testFunction = (req, res) => {
+  const headers = req.getAllHeaders();
+  // This is a useless function
+  res.send('response');
+  return true;
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if (testFunction(req, res)) {
+    console.log("Cool beans");
+  }
 });
 
 module.exports = router;
